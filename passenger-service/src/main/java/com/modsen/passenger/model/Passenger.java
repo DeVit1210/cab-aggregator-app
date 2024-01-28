8dc805ec2f5c69b179cbc413794681dc94644a7f
DEN <mozolden7@gmail.com>
2024-01-28 18:57:44 +0300

    refactor: add changelog to change field name

--- passenger-service/src/main/java/com/modsen/passenger/model/Passenger.java
+++ passenger-service/src/main/java/com/modsen/passenger/model/Passenger.java
@@ -21,8 +21,8 @@ public class Passenger {
     private Long id;
     @Column(name = "first_name")
     private String firstName;
-    @Column(name = "second_name")
-    private String secondName;
+    @Column(name = "last_name")
+    private String lastName;
     @Column(name = "phone_number")
     private String phoneNumber;
     @Column(name = "email")
