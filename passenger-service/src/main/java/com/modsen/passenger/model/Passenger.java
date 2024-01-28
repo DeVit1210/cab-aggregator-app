3428eba16fcdea92796516af364ccd3e37c57eb3
DEN <mozolden7@gmail.com>
2024-01-28 15:54:56 +0300

    feat: configure application.properties and create passenger model

---
+++ passenger-service/src/main/java/com/modsen/passenger/model/Passenger.java
@@ -0,0 +1,39 @@
+package com.modsen.passenger.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "passengers")
+public class Passenger {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "second_name")
+    private String secondName;
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+    @Column(name = "email", unique = true)
+    private String email;
+}
+
+
+
+
+
+
+
+
+
---
+++ passenger-service/src/main/java/com/modsen/passenger/repository/PassengerRepository.java
@@ -0,0 +1,10 @@
+package com.modsen.passenger.repository;
+
+import com.modsen.passenger.model.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+
+}
