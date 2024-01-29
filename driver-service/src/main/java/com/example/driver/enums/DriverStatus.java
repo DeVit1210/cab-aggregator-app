5febb7e579550c593b68a2220b1232a276349b99
DEN <mozolden7@gmail.com>
2024-01-29 09:42:39 +0300

    feat: create models and liquibase changelog

---
+++ driver-service/src/main/java/com/example/driver/enums/DriverStatus.java
@@ -0,0 +1,8 @@
+package com.example.driver.enums;
+
+public enum DriverStatus {
+    OFFLINE,
+    ON_TRIP,
+    WAITING_FOR_PASSENGER,
+    AVAILABLE
+}
---
+++ driver-service/src/main/java/com/example/driver/model/Driver.java
@@ -0,0 +1,38 @@
+package com.example.driver.model;
+
+import com.example.driver.enums.DriverStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "drivers")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "licence_number")
+    private String licenceNumber;
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private DriverStatus status;
+}
