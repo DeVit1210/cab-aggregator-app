cd97b03d53c28518d6814b2fbd13dae376ee7214
DEN <mozolden7@gmail.com>
2024-02-14 17:31:55 +0300

    feat: add error decoder and base exceptions in ride-service

--- ride-service/src/main/java/com/modsen/ride/exception/NoAvailableDriverForRide.java
+++ ride-service/src/main/java/com/modsen/ride/exception/NoAvailableDriverForRide.java
@@ -1,8 +1,9 @@
 package com.modsen.ride.exception;
 
 import com.modsen.ride.constants.MessageTemplates;
+import com.modsen.ride.exception.base.BadRequestException;
 
-public class NoAvailableDriverForRide extends RuntimeException {
+public class NoAvailableDriverForRide extends BadRequestException {
     public NoAvailableDriverForRide(Long rideId) {
         super(String.format(MessageTemplates.NO_AVAILABLE_DRIVER_FOR_RIDE.getValue(), rideId));
     }
--- ride-service/src/main/java/com/modsen/ride/exception/NotFinishedRideAlreadyExistsException.java
+++ ride-service/src/main/java/com/modsen/ride/exception/NotFinishedRideAlreadyExistsException.java
@@ -1,8 +1,9 @@
 package com.modsen.ride.exception;
 
 import com.modsen.ride.constants.MessageTemplates;
+import com.modsen.ride.exception.base.ConflictException;
 
-public class NotFinishedRideAlreadyExistsException extends RuntimeException {
+public class NotFinishedRideAlreadyExistsException extends ConflictException {
     public NotFinishedRideAlreadyExistsException(Long passengerId) {
         super(String.format(MessageTemplates.NOT_FINISHED_RIDE_EXISTS_FOR_PASSENGER.getValue(), passengerId));
     }
