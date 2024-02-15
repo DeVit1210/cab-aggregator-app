fa786436d2716c1beeecdeb1962639fc1958b428
DEN <mozolden7@gmail.com>
2024-02-15 13:15:57 +0300

    fix: minor changes

--- ride-service/src/main/java/com/modsen/ride/service/feign/DriverServiceClient.java
+++ ride-service/src/main/java/com/modsen/ride/service/feign/DriverServiceClient.java
@@ -3,13 +3,15 @@ package com.modsen.ride.service.feign;
 import com.modsen.ride.config.FeignConfig;
 import com.modsen.ride.constants.ServiceMappings;
 import com.modsen.ride.dto.response.DriverAvailabilityResponse;
+import com.modsen.ride.service.feign.fallback.DriverServiceClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(
         name = ServiceMappings.ServiceName.DRIVER_SERVICE,
         configuration = FeignConfig.class,
-        url = ServiceMappings.BaseUrl.DRIVER_SERVICE
+        url = ServiceMappings.BaseUrl.DRIVER_SERVICE,
+        fallback = DriverServiceClientFallback.class
 )
 public interface DriverServiceClient {
     @GetMapping(ServiceMappings.Url.DRIVER_AVAILABILITY_URL)
