4d1410d8a4bfd7b8ae0f2a2fa86e20f85d9d5c1e
DEN <mozolden7@gmail.com>
2024-02-29 00:04:28 +0300

    feat: integration tests for rating-service

---
+++ rating-service/src/test/resources/insert-ratings-data.sql
@@ -0,0 +1,8 @@
+insert into ratings (id, rated_person_id, role, rating_value, comment, ride_id)
+values (1, 1, 'DRIVER', 'FIVE', 'wonderful ride!', 1);
+insert into ratings (id, rated_person_id, role, rating_value, comment, ride_id)
+values (2, 1, 'DRIVER', 'ONE', 'terrible ride!', 2);
+insert into ratings (id, rated_person_id, role, rating_value, comment, ride_id)
+values (3, 1, 'DRIVER', 'FOUR', 'not bad', 3);
+insert into ratings (id, rated_person_id, role, rating_value, comment, ride_id)
+values (4, 1, 'PASSENGER', 'FIVE', 'awesome passenger!', 1);
