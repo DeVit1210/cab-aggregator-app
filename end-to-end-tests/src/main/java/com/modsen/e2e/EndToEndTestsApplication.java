778f4456b09e8e2b7d6da6fa349fbb53f01cc83e
DEN <mozolden7@gmail.com>
2024-03-07 12:18:36 +0300

    init: end-to-end-tests service

---
+++ end-to-end-tests/src/main/java/com/modsen/e2e/EndToEndTestsApplication.java
@@ -0,0 +1,13 @@
+package com.modsen.e2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndToEndTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndToEndTestsApplication.class, args);
+    }
+
+}
